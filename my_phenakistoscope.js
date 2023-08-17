ea6bda31052e8eeca9d9f3f888999e8b2a0d831e
Bianca <bianca.ropiha@icloud.com>
2023-08-17 12:13:33 +1200

    Testing how the code works for PBA

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,49 +1,52 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 12;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(OUTPUT_GIF(1000));
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 247, 203, 211);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+
+  var layer1 = new PLayer(circles);
+  layer1.mode(RING);
+  layer1.set_boundary( 0, 800 );
 
   var layer2 = new PLayer(squares);
   layer2.mode( RING );
   layer2.set_boundary( 0, 400 );
+
 }
 
-function faces(x, y, animation, pScope){
+function circles(x, y, animation, pScope){
+  //translate(50 * animation.frame, 0);
+  //scale(animation.frame*2);
+  fill(162, 232, 211)
   
-  scale(animation.frame*2);
-
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  let ballSize  = 50 + (animation.wave(1)* 20)
+  let bounce = 100* animation.wave()
+  ellipse(0, 600+bounce ,ballSize); 
+  ellipse(100, 600-bounce ,ballSize); 
 
 }
+  
+  function squares(x, y, animation, pScope){
 
-function squares(x, y, animation, pScope){
+    // this is how you set up a background for a specific layer
+    let angleOffset = (360 / SLICE_COUNT) / 2
+    let backgroundArcStart = 270 - angleOffset;
+    let backgroundArcEnd = 270 + angleOffset;
+  
+    fill(219, 103, 124)
+    arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  
+    fill(162, 232, 211)
+    ellipse(-10,-200-animation.wave()*50,50,50) // .wave is a cosine wave btw
+  
+  }
 
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
-
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-
-}
